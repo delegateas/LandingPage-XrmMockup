ddf5905367ffd1c9511e893bad245c5b8a5b1a00
jonatangranner <jonatangranner@gmail.com>
2018-09-27 14:52:39 +0200

    updated gulp to remove dependencies on packages with security vulnerabilities

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ var banner = ['/*!\n',
 ].join('');
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
+gulp.task('vendor', function(done) {
 
   // Bootstrap
   gulp.src([
@@ -56,6 +56,7 @@ gulp.task('vendor', function() {
     ])
     .pipe(gulp.dest('./vendor/magnific-popup'))
 
+  done();
 });
 
 // Compile SCSS
@@ -68,7 +69,7 @@ gulp.task('css:compile', function() {
 });
 
 // Minify CSS
-gulp.task('css:minify', ['css:compile'], function() {
+gulp.task('css:minify', gulp.series('css:compile', function() {
   return gulp.src([
       './css/*.css',
       '!./css/*.min.css'
@@ -79,10 +80,10 @@ gulp.task('css:minify', ['css:compile'], function() {
     }))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
-});
+}));
 
 // CSS
-gulp.task('css', ['css:compile', 'css:minify']);
+gulp.task('css', gulp.series('css:compile', 'css:minify'));
 
 // Minify JavaScript
 gulp.task('js:minify', function() {
@@ -99,10 +100,10 @@ gulp.task('js:minify', function() {
 });
 
 // JS
-gulp.task('js', ['js:minify']);
+gulp.task('js', gulp.series('js:minify'));
 
 // Default task
-gulp.task('default', ['css', 'js', 'vendor']);
+gulp.task('default', gulp.series('css', 'js', 'vendor'));
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -114,8 +115,9 @@ gulp.task('browserSync', function() {
 });
 
 // Dev task
-gulp.task('dev', ['css', 'js', 'browserSync'], function() {
-  gulp.watch('./scss/*.scss', ['css']);
-  gulp.watch('./js/*.js', ['js']);
-  gulp.watch('./*.html', browserSync.reload);
-});
+gulp.task('dev', gulp.parallel('css', 'js', 'browserSync', function(done) {
+  gulp.watch('./scss/*.scss').on("change", gulp.series('css'));
+  gulp.watch(['./js/*.js', '!./js/*.min.js']).on('change', gulp.series('js'));
+  gulp.watch('./*.html').on('change', gulp.series(browserSync.reload));
+  done();
+}));
